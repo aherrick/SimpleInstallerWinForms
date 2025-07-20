588e9778c13382662515629026b77ab8b24aeedb
Andrew Herrick <aj@andrewherrick.com>
2025-07-20 11:21:47 -0400

    update helper fun

---
+++ UpdateHelper/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+
+int processId = int.Parse(args[0]);
+string installerPath = args[1];
+
+// Wait for the main app to fully exit
+try
+{
+    var proc = Process.GetProcessById(processId);
+    proc.WaitForExit();
+}
+catch
+{ /* Already exited */
+}
+
+Thread.Sleep(300); // Extra safety
+
+Process.Start(new ProcessStartInfo { FileName = installerPath, UseShellExecute = true });
